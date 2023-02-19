d6dd74ff55929c6cd4a5443d0935ceadc60263cd
skwidhubz <tommaxwellparker@gmail.com>
2023-02-19 21:42:54 +1100

    bcs changes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 // const api = require('./index');
-const PORT = 3001;
+const PORT = 8080;
 
 const app = express();
 
