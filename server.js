85c318e5827e0b1b9b8e3b2b02d0d6a7dd4049d2
skwidhubz <tommaxwellparker@gmail.com>
2023-02-19 21:59:26 +1100

    commit for eric

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 // const api = require('./index');
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
